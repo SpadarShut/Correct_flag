3515b9cbab2b37c96ddff90bf1cb6de85370cb0f
SpadarShut <sp.shut@gmail.com>
2016-01-22 15:48:28 +0300

    rename sciah.js content,js

---
+++ source/content.js
@@ -0,0 +1,32 @@
+(function () {
+
+  function addCSS(CSS) {
+    var styleEl = window.document.createElement('style');
+    var styles  = window.document.createTextNode(CSS);
+    styleEl.setAttribute('title', 'Correct Flag');
+    styleEl.appendChild(styles);
+    window.document.head && window.document.head.appendChild(styleEl);
+  };
+
+  function setFavicon(icon) {
+    var faviconEl = document.querySelector('link[rel=icon]');
+
+    if (!faviconEl) {
+      faviconEl = document.createElement('link');
+      faviconEl.setAttribute('rel', 'icon');
+      document.head.appendChild(faviconEl);
+    }
+    faviconEl.setAttribute('href', icon );
+  }
+
+  chrome.runtime.sendMessage({'domain': location.host}, function(fixes){
+    if (fixes.css || fixes.favicon) {
+      window.addEventListener('DOMContentLoaded', function() {
+        fixes.css && addCSS(fixes.css);
+        fixes.favicon && setFavicon(fixes.favicon);
+
+      })
+    }
+  });
+
+})();
